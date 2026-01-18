31c8c477f5674cfa453e58915b9d410da014d806
sofia <sofiaafatima20@gmail.com>
2026-01-18 16:56:01 +0530

    feat: complete full-stack integration with firebase

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  // Replace these with the keys from your Firebase Web App settings
-  apiKey: "YOUR_API_KEY",
+  apiKey: "AIzaSyAAmMxROBQCu9J59U2ZeKzDlBeU3a7bRsE",
   authDomain: "provia-platform.firebaseapp.com",
-  databaseURL: "https://provia-platform-default-rtdb.firebaseio.com/", 
   projectId: "provia-platform",
-  storageBucket: "provia-platform.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  storageBucket: "provia-platform.firebasestorage.app",
+  messagingSenderId: "503916799653",
+  appId: "1:503916799653:web:a1b49f1f3a0c4e5fc10f4c"
 };
 
 const app = initializeApp(firebaseConfig);
